50a7d259cf4545535fdd10c6c61749061379d9f7
Dylan Jordan <kyle.dylan.jordan@gmail.com>
2022-03-24 00:13:19 -0400

    commit tree 386e7ab2458a58145421ef4647a81a54ef8da285

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(names){
+    const nameArray = [];
+    for (let i = 0; i < names.length; i++){
+        nameArray.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return nameArray;
+}
+
+function countDown(posint){
+    while (posint >= 0){
+        console.log(posint);
+        posint--;
+    }
+}
